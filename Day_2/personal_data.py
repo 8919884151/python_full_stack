ef177ff46103f0f5d7d3d85dbfb9371dec32b5dc
Karla Keerthi <146101640+8919884151@users.noreply.github.com>
2026-03-06 10:14:36 +0530

    Add files via upload

---
+++ Day_2/personal_data.py
@@ -0,0 +1,35 @@
+Student_Name='Karla Keerthi'
+Student_ID='CGH2947'
+Batch_No='PFS-HYD-050'
+Email_ID='karlakeerthi44@gmail.com'
+Date_of_Birth='2004-01-06'
+Age=22
+Gender='Female'
+Blood_Group='A+'
+City='Hyderabad'
+State='Telangana'
+Student_Phone_Number='+918919884151'
+Parent_Phone_Number='+919848919529'
+Github_Link='https://github.com/8919884151'
+#Academic Information
+College_Name='Keshav memorial institute of technology'
+USN_Number='21bd1a1292'
+Qualification='UG (Bachelor Degrees)'
+Department='B.Tech - Information Technology (IT)'
+Pass_out_Year=2025
+Graduation_Percentage='90%'
+Arrears='No'
+No_of_Arrears=0
+Tenth_Pass_Year=2019
+Tenth_Percentage='90%'
+Twelveth_Pass_Year=2021
+Twelveth_Percentage='96%'
+Skills="Python, Flask, HTML, CSS, Bootstrap, Javascript, MySQL"
+print("Personal information")
+print(f"Student name={Student_Name}, Student id={Student_ID}, Batch no={Batch_No}, Email id={Email_ID}")
+print(f"Date fo birth={Date_of_Birth}, Age={Age},Gender={Gender}")
+print(f"Student phone number={Student_Phone_Number},Parent phone number={Parent_Phone_Number}")
+print(f"Github link={Github_Link}")
+
+      
+
