b9d0ca6cb34549bedf5b6b063260aa8301da90c0
Karla Keerthi <146101640+8919884151@users.noreply.github.com>
2026-03-10 17:50:13 +0530

    Add files via upload

---
+++ Day_5/loop_statements.py
@@ -0,0 +1,28 @@
+#for loop
+'''products=["milk","bread","butter","jam"]
+for item in products:
+    print(item)
+
+products={'milk':30,'bread':43,'butter':90,'jam':120}
+for item in products:
+    print(item,products[item])
+
+name="keerthi"
+for i in name:
+    print(i,end="")
+
+
+i=1
+for i in range(0,10,3):
+    print(i)'''
+
+#table
+n=int(input("Enter number: "))
+for i in range(1,21):
+    print(f'{n}*{i}={n*i}')
+    
+
+
+
+
+    
